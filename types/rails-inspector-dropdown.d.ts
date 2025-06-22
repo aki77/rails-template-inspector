dbae679952af94305290f290b4a8a7c4330a0f5e
aki <lala.akira@gmail.com>
2025-06-23 06:39:43 +0900

    fix: build

--- types/rails-inspector-dropdown.d.ts
+++ types/rails-inspector-dropdown.d.ts
@@ -1,18 +1,17 @@
 import { LitElement } from 'lit';
-
 export declare class RailsInspectorDropdown extends LitElement {
-  parentPaths: readonly string[];
-  currentPath: string;
-  private dropdownOpen;
-  render(): import("lit").TemplateResult<1>;
-  private _toggleDropdown;
-  private _handleBlur;
-  private _handleDropdownItemClick;
-  private _dispatchOpen;
+    static styles: import("lit").CSSResult;
+    parentPaths: readonly string[];
+    currentPath: string;
+    private dropdownOpen;
+    render(): import("lit").TemplateResult<1>;
+    private _toggleDropdown;
+    private _handleBlur;
+    private _handleDropdownItemClick;
+    private _dispatchOpen;
 }
-
 declare global {
-  interface HTMLElementTagNameMap {
-    'rails-inspector-dropdown': RailsInspectorDropdown;
-  }
+    interface HTMLElementTagNameMap {
+        'rails-inspector-dropdown': RailsInspectorDropdown;
+    }
 }
